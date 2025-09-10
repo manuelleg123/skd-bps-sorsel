23dd12989166b0cebbbb4adb040733f0bb50826e
unknown <emanuellega123@gmail.com>
2025-09-10 16:19:12 +0900

    memperbaiki view user

--- app/Views/dashboard/users.php
+++ app/Views/dashboard/users.php
@@ -4,8 +4,9 @@
 <div class="row">
     <div class="col-lg-12 d-flex align-items-stretch">
         <div class="card w-100">
-            <div class="card-header">
+            <div class="card-header d-flex justify-content-between">
                 <label class="col-form-label">Data Admin</label>
+                <button type="button" class="btn btn-primary" id="addAdminBtn" onclick="showAddAdminModal()">Tambah Admin</button>
             </div>
             <div class="card-body">
                 <div class="table-responsive">
@@ -25,3 +26,4 @@
     </div>
 </div>
 <?php $this->endSection(); ?>
+
